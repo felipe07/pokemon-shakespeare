ab76065ea65e4fd645a9277a3ef67a76b4b85679
felipe07 <felipe07@gmail.com>
2020-06-14 02:40:26 +0200

    Add error handling if pokemon not found

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-use actix_web::{web, Result, App, HttpResponse, HttpServer, Responder};
-use actix_web::get;
+use actix_http::ResponseBuilder;
+use actix_web::{get, web, App, error, http::header, http::StatusCode, HttpResponse, HttpServer, Result};
+use failure::Fail;
 use serde::{Deserialize, Serialize};
+use pokerust::{PokemonSpecies, FromName};
+
+#[derive(Serialize)]
+#[derive(Fail, Debug)]
+enum PokemonServiceError {
+    #[fail(display = "bad request")]
+    PokemonNotFound,
+    #[fail(display = "internal error")]
+    InternalError
+}
+
+impl error::ResponseError for PokemonServiceError {
+    fn error_response(&self) -> HttpResponse {
+        ResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            PokemonServiceError::PokemonNotFound => StatusCode::NOT_FOUND,
+            PokemonServiceError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+type DescriptionResult<T> = Result<T, PokemonServiceError>;
 
 #[derive(Serialize, Deserialize)]
 struct PokemonDescription {
@@ -8,30 +36,35 @@ struct PokemonDescription {
     description: String
 }
 
-#[get("/hello")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+async fn get_description(name: String) -> DescriptionResult<PokemonDescription> {
+    let poke = match PokemonSpecies::from_name(&name) {
+        Ok(poke) => poke,
+        Err(_err) => return Err(PokemonServiceError::PokemonNotFound),
+    };
 
-async fn get_description(name: String) -> PokemonDescription {
-    PokemonDescription {
+    let descs = poke.flavor_text_entries;
+    println!("{:?}", descs);
+    
+    Ok(PokemonDescription {
         name,
         description: "Mocked pokemon's description".to_string()
-    }
+    })
 }
 
 #[get("/pokemon/{name}")]
-async fn description(name: web::Path<String>) -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok().json(
-        get_description(name.to_string()).await))
+async fn description(name: web::Path<String>) -> Result<HttpResponse, PokemonServiceError> {
+    let result = get_description(name.to_string()).await;
+    match result {
+        Ok(desc) => Ok(HttpResponse::Ok().json(desc)),
+        Err(err) => Err(err),
+    } 
+    
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(index)
-            .service(description)
+        App::new().service(description)
     })
     .bind("127.0.0.1:8088")?
     .run()
